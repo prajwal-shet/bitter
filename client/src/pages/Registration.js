f30b7ac8092b194852a8feeff74503ee708cd3ec
prajwal <prajwalshet68@gmail.com>
2024-07-09 12:50:56 +0530

    first commit

---
+++ client/src/pages/Registration.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+function Registration() {
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().min(3).max(15).required(),
+    password: Yup.string().min(4).max(20).required(),
+  });
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/auth", data).then((response) => {
+        if(response.data.error)
+            {
+                alert("user already exists")
+            }
+      console.log(response.data);
+    });
+  };
+
+  return (
+    <div>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+          <label>Username: </label>
+          <ErrorMessage name="username" component="span" />
+          <Field
+            autocomplete="on"
+            id="inputCreatePost"
+            name="username"
+            placeholder="(Ex. John123...)"
+          />
+
+          <label>Password: </label>
+          <ErrorMessage name="password" component="span" />
+          <Field
+            autocomplete="on"
+            type="password"
+            id="inputCreatePost"
+            name="password"
+            placeholder="Your Password..."
+          />
+
+          <button type="submit"> Register</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default Registration;
