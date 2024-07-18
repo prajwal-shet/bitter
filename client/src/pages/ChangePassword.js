491ccacac4463329be42457fcedb4263d37a36e9
prajwal v shet <148465811+prajwal-shet@users.noreply.github.com>
2024-07-18 14:46:09 +0530

    Update ChangePassword.js

--- client/src/pages/ChangePassword.js
+++ client/src/pages/ChangePassword.js
@@ -8,7 +8,7 @@ function ChangePassword() {
   const changePassword = () => {
     axios
       .put(
-        "http://localhost:3001/auth/changepassword",
+        "https://bitter-backend-production.up.railway.app/auth/changepassword",
         {
           oldPassword: oldPassword,
           newPassword: newPassword,
