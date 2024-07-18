528f25cec6f04bd2704b53d8e381fc9121551734
prajwal v shet <148465811+prajwal-shet@users.noreply.github.com>
2024-07-18 14:46:55 +0530

    Update CreatePost.js

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -26,7 +26,7 @@ function CreatePost() {
 
   const onSubmit = (data) => {
     axios
-      .post("http://localhost:3001/posts", data, {
+      .post("https://bitter-backend-production.up.railway.app/posts", data, {
         headers: { accessToken: localStorage.getItem("accessToken") },
       })
       .then((response) => {
