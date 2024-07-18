c527d15d143e7dc80be27828e4deca9cfc5c3125
prajwal v shet <148465811+prajwal-shet@users.noreply.github.com>
2024-07-18 14:49:53 +0530

    Update Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -11,11 +11,11 @@ function Profile() {
   const { authState } = useContext(AuthContext);
 
   useEffect(() => {
-    axios.get(`http://localhost:3001/auth/basicinfo/${id}`).then((response) => {
+    axios.get(`https://bitter-backend-production.up.railway.app/auth/basicinfo/${id}`).then((response) => {
       setUsername(response.data.username);
     });
 
-    axios.get(`http://localhost:3001/posts/byuserId/${id}`).then((response) => {
+    axios.get(`https://bitter-backend-production.up.railway.app/posts/byuserId/${id}`).then((response) => {
       setListOfPosts(response.data);
     });
   }, []);
